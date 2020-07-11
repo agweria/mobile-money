2244bb2759f4d0b91cb0a267a1aac4147f902a16
Samuel Dervis <sam@agweria.com>
2020-07-12 02:35:47 +0300

    docs: update color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,22 @@ module.exports = {
     home: true,
     serviceWorker: true,
     head: [
-        ['link', {rel: 'icon', href: `/favicon.ico`}],
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#c970b6'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }]
     ],
     plugins: [
         '@vuepress/plugin-back-to-top',
